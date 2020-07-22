28550eabb14d6240e17da2dc87cde22308c752e8
geeth22 <geethchandra22@gmail.com>
2020-07-22 13:39:34 +0530

    update commit

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,4 +1,3 @@
-// UC 3 -> Adding Part Time Employee and Wage
 
 public class EmpWageBuilderUC3 {
 	public static void main(String[] args) {
@@ -17,6 +16,33 @@ public class EmpWageBuilderUC3 {
 		empHrs=4;
 	else
 		empHrs=0;
+	empWage=empHrs*EMP_RATE_PER_HOUR;
+	System.out.println("Employee Wage is " + empWage);
+	}
+}
+// UC 4 -> Solving Using Case Statement
+
+public class EmpWageBuilderUC4 { 
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	
+	public static void main(String[] args) {
+		// VARIABLES
+		int empHrs=0;
+		int empWage=0;
+		// COMPUTATION 
+		double EmpCheck = Math.floor(Math.random() * 10) % 3;
+	switch (empCheck) {
+		case IS_PART_TIME:
+			empHrs=4;
+			break;
+		case IS_FULL_TIME:
+			empHrs=8:
+			break;
+		default:
+			empHrs=0;
+	}
 	empWage=empHrs*EMP_RATE_PER_HOUR;
 	System.out.println("Employee Wage is " + empWage);
 	}
