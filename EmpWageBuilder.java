e976a9a9fd7ee18bd9d545fc2e68441385a7ebc3
geeth22 <geethchandra22@gmail.com>
2020-07-22 13:30:00 +0530

    update commit

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,2 +1,14 @@
-Welcome to Employee Wage Computation Program on Master Branch
+// UC 1 -> whether employ present or absent 
 
+public class EmpWageBuilderUC1 {
+	public static void main(String[] args) {
+		// CONSTANTS 
+		int IS_FULL_TIME=1;
+		// COMPUTATION 
+		double EmpCheck = Math.floor(Math.random() * 10) % 2;
+	if (EmpCheck==IS_FULL_TIME)
+		System.out.println("Employ is Present");
+	else
+		System.out.println("Employ is Absent");
+	}
+}
