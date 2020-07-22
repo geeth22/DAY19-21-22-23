bc8e32effab23841bb8980df3b31ff776c0cf53f
geeth22 <geethchandra22@gmail.com>
2020-07-22 13:34:26 +0530

    update commit

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,14 +1,20 @@
-// UC 1 -> whether employ present or absent 
+// UC 2 -> Calculate Daily Employee Wage
 
-public class EmpWageBuilderUC1 {
+public class EmpWageBuilderUC2 {
 	public static void main(String[] args) {
 		// CONSTANTS 
 		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		// VARIABLES
+		int empHrs=0;
+		int empWage=0;
 		// COMPUTATION 
 		double EmpCheck = Math.floor(Math.random() * 10) % 2;
 	if (EmpCheck==IS_FULL_TIME)
-		System.out.println("Employ is Present");
+		empHrs=8;
 	else
-		System.out.println("Employ is Absent");
+		empHrs=0;
+	empWage=empHrs*EMP_RATE_PER_HOUR;
+	System.out.println("Employee Wage is " + empWage);
 	}
 }
