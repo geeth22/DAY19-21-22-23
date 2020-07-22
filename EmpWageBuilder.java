e69412713711d327dba7ea396b0b974cd5eb79e3
geeth22 <geethchandra22@gmail.com>
2020-07-22 13:44:06 +0530

    update commit

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,38 +1,18 @@
+// UC 5 -> Calculating Wages for a Month
 
-public class EmpWageBuilderUC3 {
-	public static void main(String[] args) {
-		// CONSTANTS 
-		int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int EMP_RATE_PER_HOUR=20;
-		// VARIABLES
-		int empHrs=0;
-		int empWage=0;
-		// COMPUTATION 
-		double EmpCheck = Math.floor(Math.random() * 10) % 3;
-	if (EmpCheck==IS_FULL_TIME)
-		empHrs=8;
-	else if (EmpCheck==PartTime)
-		empHrs=4;
-	else
-		empHrs=0;
-	empWage=empHrs*EMP_RATE_PER_HOUR;
-	System.out.println("Employee Wage is " + empWage);
-	}
-}
-// UC 4 -> Solving Using Case Statement
-
-public class EmpWageBuilderUC4 { 
+public class EmpWageBuilderUC5 { 
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
-	
+	public static final int NUM_OF_WORKING_DAYS=20;
 	public static void main(String[] args) {
 		// VARIABLES
 		int empHrs=0;
 		int empWage=0;
+		int totalEmpWage=0;
 		// COMPUTATION 
-		double EmpCheck = Math.floor(Math.random() * 10) % 3;
+	for (int day=0; day < NUM_OF_WORKING_DAYS; day++) {
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 	switch (empCheck) {
 		case IS_PART_TIME:
 			empHrs=4;
@@ -42,8 +22,11 @@ public class EmpWageBuilderUC4 {
 			break;
 		default:
 			empHrs=0;
-	}
+		}
 	empWage=empHrs*EMP_RATE_PER_HOUR;
+	totalEmpWage+ = empWage;
 	System.out.println("Employee Wage is " + empWage);
+	}
+	System.out.println("total Employee Wage is " + totalEmpWage);
 	}
 }
