0f3c89690eb1261979ec47d071870dcb1d471963
Geeth <66339384+geeth22@users.noreply.github.com>
2020-07-22 13:41:43 +0530

    Update EmpWageBuilder.java

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,25 +1,3 @@
-
-public class EmpWageBuilderUC3 {
-	public static void main(String[] args) {
-		// CONSTANTS 
-		int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int EMP_RATE_PER_HOUR=20;
-		// VARIABLES
-		int empHrs=0;
-		int empWage=0;
-		// COMPUTATION 
-		double EmpCheck = Math.floor(Math.random() * 10) % 3;
-	if (EmpCheck==IS_FULL_TIME)
-		empHrs=8;
-	else if (EmpCheck==PartTime)
-		empHrs=4;
-	else
-		empHrs=0;
-	empWage=empHrs*EMP_RATE_PER_HOUR;
-	System.out.println("Employee Wage is " + empWage);
-	}
-}
 // UC 4 -> Solving Using Case Statement
 
 public class EmpWageBuilderUC4 { 
