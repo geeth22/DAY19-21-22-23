d5b955587e59988930f066215c72086eb56e6144
geeth22 <geethchandra22@gmail.com>
2020-07-22 13:36:45 +0530

    update commit

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,17 +1,20 @@
-// UC 2 -> Calculate Daily Employee Wage
+// UC 3 -> Adding Part Time Employee and Wage
 
-public class EmpWageBuilderUC2 {
+public class EmpWageBuilderUC3 {
 	public static void main(String[] args) {
 		// CONSTANTS 
-		int IS_FULL_TIME=1;
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
 		int EMP_RATE_PER_HOUR=20;
 		// VARIABLES
 		int empHrs=0;
 		int empWage=0;
 		// COMPUTATION 
-		double EmpCheck = Math.floor(Math.random() * 10) % 2;
+		double EmpCheck = Math.floor(Math.random() * 10) % 3;
 	if (EmpCheck==IS_FULL_TIME)
 		empHrs=8;
+	else if (EmpCheck==PartTime)
+		empHrs=4;
 	else
 		empHrs=0;
 	empWage=empHrs*EMP_RATE_PER_HOUR;
