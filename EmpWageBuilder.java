811356bd904476bf46e1229631dd2aff861d618e
geeth22 <geethchandra22@gmail.com>
2020-07-22 15:14:44 +0530

    update commit

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,15 +1,19 @@
-// Uc 12 -> Refactor to have list of multiple companies to manage Employee Wage
+// uc 13 -> Store the Daily Wage along with the Total Wage
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class CalculateEmpWageUsingList {
+public class StoreDailyWage {
         
         public static final int IS_PART_TIME = 1;
         public static final int IS_FULL_TIME = 2;
         public static final int MAX_HRS_IN_MONTH = 10;
         public static final int NUM_OF_WORKING_DAYS = 10;
         public static List<CompanyWageObj> companyWageLIst = new ArrayList<CompanyWageObj>();
+	public static int[] dailyWage = new  int[4];
         
         static {
             companyWageLIst.add(new CompanyWageObj());
@@ -23,11 +27,12 @@ public class CalculateEmpWageUsingList {
         }
         
         public static void main(String[] args) {
-            computeEmpWage();
+            storeDailyWageWithTotalWage();
         }
-        public static void computeEmpWage(){
+        public static void storeDailyWageWithTotalWage(){
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0,employeeWage = 0;
-            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+		int dailywage = 0;
+            while(totalWorkingDays < NUM_OF_WORKING_DAYS){
                 totalWorkingDays++;
                 for(int i = 0; i< 4 ; i++){
                     int empCheck = (int)(Math.floor(Math.random()*10)%3);
@@ -41,11 +46,14 @@ public class CalculateEmpWageUsingList {
                     default:
                         empHrs = 0;
                     }
-                    employeeWage += empHrs * companyWageLIst.get(i).EMPOYEE_RATE_PER_HOUR;
-                }
+                    dailywage = empHrs * companyWageLIst.get(i).EMPOYEE_RATE_PER_HOUR;
+                dailyWage[i] = dailywage;
+                employeeWage += dailywage;
             }
-            System.out.println(employeeWage);
-            
-            return ;
         }
+        System.out.println(employeeWage);
+        System.out.println(Arrays.toString(dailyWage));
+        
+        return ;
+    }
 }
