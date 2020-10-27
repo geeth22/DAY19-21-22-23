08b95e01f35d0416ea3a16b4aff0001626718658
geeth22 <geethchandra22@gmail.com>
2020-10-27 09:25:23 +0530

    Ability to save the total wage for each company

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,58 +1,58 @@
-// UC 14 -> Ability to get the Total Wage when queried by Company
+// UC 9 - Compute Employee Wage and Save Total Wage by Company
 
-import java.util.ArrayList;
-import java.util.List;
+public class EmpWageBuilderObject {
+	// Constants
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
 
-public class QueryByCompanyName {
-        
-        public static final int IS_PART_TIME = 1;
-        public static final int IS_FULL_TIME = 2;
-        public static final int MAX_HRS_IN_MONTH = 10;
-        public static final int NUM_OF_WORKING_DAYS = 10;
-        public static List<CompanyWageObj> companyWageLIst = new ArrayList<CompanyWageObj>();
-        
-        static {
-        	companyWageLIst.add(new CompanyWageObj());
-	        companyWageLIst.get(0).EMPOYEE_RATE_PER_HOUR = 20;
-        	companyWageLIst.get(0).setCompanyName("Microsoft");
-        	companyWageLIst.add(new CompanyWageObj());
-        	companyWageLIst.get(1).EMPOYEE_RATE_PER_HOUR = 30;
-        	companyWageLIst.get(1).setCompanyName("Amazon");
-        	companyWageLIst.add(new CompanyWageObj());
-        	companyWageLIst.get(2).EMPOYEE_RATE_PER_HOUR = 40;
-        	companyWageLIst.get(2).setCompanyName("Google");
-        	companyWageLIst.add(new CompanyWageObj());
-        	companyWageLIst.get(3).EMPOYEE_RATE_PER_HOUR = 50;
-        	companyWageLIst.get(3).setCompanyName("Apple");
-        }
-  public static void main(String[] args) {
-        computeEmpWage("Apple");
-    }
-    public static void computeEmpWage(String companyName){
-        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0,employeeWage = 0;
-        while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
-            totalWorkingDays++;
-            for(int i = 0; i< 4 ; i++){
-                if(companyName.equalsIgnoreCase(companyWageLIst.get(i).getCompanyName())){
-                    int empCheck = (int)(Math.floor(Math.random()*10)%3);
-                    switch (empCheck) {
-                    case IS_PART_TIME:
-                        empHrs = 2;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                    }
-                    employeeWage += empHrs * companyWageLIst.get(i).EMPOYEE_RATE_PER_HOUR;
-                    break;
-                }
-                
-            }
-        }
-        System.out.println(employeeWage);
-        
-        return ;
-    }
+	private final string company;
+	private final int EMP_RATE_PER_HOUR; 
+	private final int NUM_OF_WORKING_DAYS;
+	private final int MAX_HRS_IN_MONTH;
+	private int totalEmpWage;
+
+	public EmpWageBuilderObject(String Company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
+	
+		this.company = company;
+		this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+		this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
+		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+	}
+
+	public void computeEmpWage() {
+		// VARIABLES
+	  	int empHrs=0;
+		int totalEmpHrs=0;
+	  	int totalWorkingDays=0;
+		// COMPUTATION 
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+	  	totalWorkingDays++;
+	  	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+		    case IS_PART_TIME:
+			    empHrs=4;
+			    break;
+		    case IS_FULL_TIME:
+			    empHrs=8:
+			    break;
+		    default:
+			    empHrs=0;
+	  	}
+		totalEmpHrs+ = empHrs;
+		System.out.println("DAY#: " + totalWorkingDays + "Emp Hr: " +empHrs);
+	}
+		totalEmpWage = totalempHrs * EMP_RATE_PER_HOUR;
 }
+
+	public String toString() {
+		return "Total Emp Wage for Company: " +company+ " is: " + totalEmpWage;
+	}
+	
+	public static void main(String[] args) {
+		EmpWageBuilderObject Dmart = new EmpWageBuilderObject("Dmart",20,2,10);
+		EmpWageBuilderObject Bestprice = new EmpWageBuilderObject("Bestprice",10,4,20);
+		Dmart.computeEmpWage();
+		System.out.println(Dmart);
+		Bestprice.computeEmpWage();
+		System.out.println(Bestprice);
+	}
