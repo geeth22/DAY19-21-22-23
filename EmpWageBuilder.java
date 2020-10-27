398bb2ff200678e739a4cdb7e1a18fe3e7f51f42
geeth22 <geethchandra22@gmail.com>
2020-10-27 09:34:45 +0530

    Compute Employee Wage for Multiple Company

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,34 +1,20 @@
-// UC 9 - Compute Employee Wage and Save Total Wage by Company
+// UC 8 -> Compute Employee Wage of multiple companies
 
-public class EmpWageBuilderObject {
-	// Constants
+public class EmpWageBuilderMultipleCompany {
+  // Constants
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 
-	private final string company;
-	private final int EMP_RATE_PER_HOUR; 
-	private final int NUM_OF_WORKING_DAYS;
-	private final int MAX_HRS_IN_MONTH;
-	private int totalEmpWage;
-
-	public EmpWageBuilderObject(String Company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
-	
-		this.company = company;
-		this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
-		this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
-		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
-	}
-
-	public void computeEmpWage() {
+	public static int computeEmpWage(String Company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
 		// VARIABLES
 	  	int empHrs=0;
-		int totalEmpHrs=0;
+		  int totalEmpHrs=0;
 	  	int totalWorkingDays=0;
 		// COMPUTATION 
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+  	while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 	  	totalWorkingDays++;
 	  	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
+     	switch (empCheck) {
 		    case IS_PART_TIME:
 			    empHrs=4;
 			    break;
@@ -38,21 +24,16 @@ public class EmpWageBuilderObject {
 		    default:
 			    empHrs=0;
 	  	}
-		totalEmpHrs+ = empHrs;
-		System.out.println("DAY#: " + totalWorkingDays + "Emp Hr: " +empHrs);
-	}
-		totalEmpWage = totalempHrs * EMP_RATE_PER_HOUR;
-}
-
-	public String toString() {
-		return "Total Emp Wage for Company: " +company+ " is: " + totalEmpWage;
+	  totalEmpHrs+ = empHrs;
+	  System.out.println("DAY#: " + totalWorkingDays + "Emp Hr: " +empHrs);
+	 }
+	  int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+	  System.out.println("total Employee Wage for company " +company+" + totalEmpWage);
+	  return totalEmpWage;
 	}
 	
-	public static void main(String[] args) {
-		EmpWageBuilderObject Dmart = new EmpWageBuilderObject("Dmart",20,2,10);
-		EmpWageBuilderObject Bestprice = new EmpWageBuilderObject("Bestprice",10,4,20);
-		Dmart.computeEmpWage();
-		System.out.println(Dmart);
-		Bestprice.computeEmpWage();
-		System.out.println(Bestprice);
+	  public static void main(String[] args) {
+		  computeEmpWage("Dmart",20,2,10);
+      computeEmpWage("Bestprice",10,4,20);
 	}
+}
